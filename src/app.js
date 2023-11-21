bf0160c946f3f00d680127ce135cf75f674cfe37
Ruslan professional <ruslan.temirbulatov001@gmail.com>
2023-11-22 00:55:03 +0500

    Решение ДЗ 1-3

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {createElement} from './utils.js';
 import './styles.css';
 
@@ -8,16 +8,30 @@ import './styles.css';
  * @returns {React.ReactElement}
  */
 function App({store}) {
+  useEffect(() => {
+    const list = store.getState().list;
+    let maxCode = 0;
+    list.map((item) => (
+      maxCode = maxCode < item.code ? item.code : maxCode
+    ))
+    store.setLastCode(maxCode)
+  }, [])
 
+  /**
+ * Функцию добавления item
+ */
+  const addItem = () => {
+    store.setLastCode(store.getState().lastCode + 1)
+    store.addItem()
+  }
   const list = store.getState().list;
-
   return (
     <div className='App'>
       <div className='App-head'>
         <h1>Приложение на чистом JS</h1>
       </div>
       <div className='App-controls'>
-        <button onClick={() => store.addItem()}>Добавить</button>
+        <button onClick={addItem}>Добавить</button>
       </div>
       <div className='App-center'>
         <div className='List'>{
@@ -26,7 +40,7 @@ function App({store}) {
               <div className={'Item' + (item.selected ? ' Item_selected' : '')}
                    onClick={() => store.selectItem(item.code)}>
                 <div className='Item-code'>{item.code}</div>
-                <div className='Item-title'>{item.title}</div>
+                <div className='Item-title'>{item.title} {item.counter && `| Выделяли ${item.counter} раз`}</div>
                 <div className='Item-actions'>
                   <button onClick={() => store.deleteItem(item.code)}>
                     Удалить
--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@
  */
 class Store {
   constructor(initState = {}) {
-    this.state = initState;
+    this.state = { ...initState, lastCode: 0 };
     this.listeners = []; // Слушатели изменений состояния
   }
 
@@ -16,8 +16,8 @@ class Store {
     this.listeners.push(listener);
     // Возвращается функция для удаления добавленного слушателя
     return () => {
-      this.listeners = this.listeners.filter(item => item !== listener);
-    }
+      this.listeners = this.listeners.filter((item) => item !== listener);
+    };
   }
 
   /**
@@ -44,9 +44,12 @@ class Store {
   addItem() {
     this.setState({
       ...this.state,
-      list: [...this.state.list, {code: this.state.list.length + 1, title: 'Новая запись'}]
-    })
-  };
+      list: [
+        ...this.state.list,
+        { code: this.state.lastCode, title: "Новая запись" },
+      ],
+    });
+  }
 
   /**
    * Удаление записи по коду
@@ -55,9 +58,9 @@ class Store {
   deleteItem(code) {
     this.setState({
       ...this.state,
-      list: this.state.list.filter(item => item.code !== code)
-    })
-  };
+      list: this.state.list.filter((item) => item.code !== code),
+    });
+  }
 
   /**
    * Выделение записи по коду
@@ -66,13 +69,26 @@ class Store {
   selectItem(code) {
     this.setState({
       ...this.state,
-      list: this.state.list.map(item => {
+      list: this.state.list.map((item) => {
         if (item.code === code) {
           item.selected = !item.selected;
+          item.counter = item.counter >= 1 ? item.counter + 1 : 1
+        } else {
+          item.selected = false;
         }
         return item;
-      })
-    })
+      }),
+    });
+  }
+  /**
+   * Сохраняем в стейт код последнего элемента
+   * @param newLastCode
+   */
+  setLastCode(newLastCode) {
+    this.setState({
+      ...this.state,
+      lastCode: newLastCode,
+    });
   }
 }
 
